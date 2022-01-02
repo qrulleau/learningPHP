bd3fc3d49f324bccfac11244518505f82b796b4b
qrulleau <quentin.rulleau@yahoo.com>
2022-01-02 19:50:20 +0100

    ADD: init repo

---
+++ class.php
@@ -0,0 +1,8 @@
+<?php
+// https://www.pierre-giraud.com/php-mysql-apprendre-coder-cours/oriente-objet-constructeur-destructeur/
+class Person {
+  public function __construct($a, $c)
+  {
+    
+  }
+}
