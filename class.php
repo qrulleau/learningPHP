206ea1e734c6fd7df9f6d94711199b2b52a2b18d
qrulleau <quentin.rulleau@yahoo.com>
2022-01-08 11:12:48 +0100

    a

--- class.php
+++
@@ -1,8 +0,0 @@
-<?php
-// https://www.pierre-giraud.com/php-mysql-apprendre-coder-cours/oriente-objet-constructeur-destructeur/
-class Person {
-  public function __construct($a, $c)
-  {
-    
-  }
-}
